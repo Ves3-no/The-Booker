aa87c46ce7ec303d6e4691cdc327aeedfe1b46f2
Ves3-no <heinebaranevestre@gmail.com>
2026-08-10 17:55:45 +0200

    kjapp

--- src/comps/AdminBookings.tsx
+++ src/comps/AdminBookings.tsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react"
+import { supabase } from "../supabase"
+import CompanyId  from "../CompanyId"
 export default function main(){
+    useEffect(()=>{
+        async function getCustumers(){
+            const { data, error } = await supabase
+                .from('custumers')
+                .select()
+                .eq('company_id', CompanyId)
+            if(error){
+                console.log(error)
+                return
+            }
+        }
+        getCustumers()
+    },[])
     return(<>
 
     </>)
