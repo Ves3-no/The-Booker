29cfba6fe0c25e946e9fd4d4628d463e4a4410dc
Heine Barane Vestre <heine26@iktfag.com>
2026-08-20 09:51:06 +0200

    skole må lagre

--- src/comps/AdminProductsCard.tsx
+++ src/comps/AdminProductsCard.tsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import type { Product } from "../types";
 
 export default function main({prod, index}: {prod: Product, index: number}){
-    const [Name, setName] = useState<string>()
-    const [Image, setImage] = useState<string>()
-    const [Price, setPrice] = useState<number>()
-    const [Description, setDescription] = useState<string>()
-    const [Stock, setStock] = useState<number>()
+    const [Name, setName] = useState<string>(prod.name)
+    const [Image, setImage] = useState<string>(prod.Image)
+    const [Price, setPrice] = useState<number>(prod.price)
+    const [Description, setDescription] = useState<string>(prod.description)
+    const [Stock, setStock] = useState<number>(prod.stock)
     return(<>
     <div key={index}>
-            <h3><input type="text" /></h3>
-            <button><img src="" alt="" /></button>
-            <p><input type="text" /></p>
-            <p><input type="text" /></p>
-            <p><input type="text" /></p>
+            <h3><input type="text" value={Name} onChange={(e)=> {setName(e.currentTarget.value)}}/></h3>
+            <button><img src={Image} alt="" /></button>
+            <p><input type="text" value={Description} onChange={(e)=> {setDescription(e.currentTarget.value)}}/></p>
+            <p><input type="text" value={Price} onChange={(e)=> {setPrice(Number(e.currentTarget.value))}}/></p>
+            <p><input type="text" value={Stock} onChange={(e)=> {setStock(Number(e.currentTarget.value))}}/></p>
             <button>Deleate</button>
     </div>
 
